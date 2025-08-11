c753e9de5710c39f5129b77fb254b76962a2d6aa
DicksonLegend <dicksyn2006@gmail.com>
2025-08-11 16:38:34 +0530

    Updated the program structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export { default as AddTaskForm } from './AddTaskForm';
 export { default as Statistics } from './Statistics';
 export { default as FilterBar } from './FilterBar';
 export { default as Layout } from './Layout';
-export { default as AuthPage } from './AuthPage';
